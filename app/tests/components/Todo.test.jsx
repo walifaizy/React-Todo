f17277d5147b292de2496ac34a90fe17a7ff825e
wal faizy <faizy.wali@gmail.com>
2017-02-21 01:37:39 +0400

    Add test suite for todo component

---
+++ app/tests/components/Todo.test.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Todo = require("Todo");
+
+describe("Todo", () =>{
+    it("should Exist",() =>{
+        expect(Todo).toExist();
+    });
+});
+

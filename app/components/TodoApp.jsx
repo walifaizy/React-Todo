ec72ba80eb7bde916336a49e65b469b815950f67
wal faizy <faizy.wali@gmail.com>
2017-02-21 01:05:21 +0400

    Render Todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,32 @@
 var React = require("react");
+var TodoList = require("TodoList");
 
 var TodoApp = React.createClass({
+    getInitialState: function() {
+        return {
+           todos: [
+               {
+                id: 1,
+                text: "Walk the dog"
+               }, {
+                id: 2,
+                text: "Clean the yard"
+               }, {
+                id: 3,
+                text: "Leave the mail on the go"
+               }, {
+                id: 4,
+                text: "Play video games"
+               }
+           ]
+        };
+    },
     render: function() {
+        var {todos} = this.state;
         return (
-            <div>TodoApp.jsx</div>
+            <div>
+                <TodoList todos={todos}></TodoList>
+            </div>
         )
     }
 });

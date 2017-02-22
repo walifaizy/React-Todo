efe534c4df91e462a2f4afb559c0cade909fae53
wal faizy <faizy.wali@gmail.com>
2017-02-22 23:25:17 +0400

    Toggling done

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -2,11 +2,17 @@ var React = require("react");
 
 
 var Todo = React.createClass({
+    /*toggle: function(id) {
+         this.props.onToggle(id);
+    },*/
     render: function() {
-        var {id,text} = this.props;
+        var {id,text,completed} = this.props;
         return (
-            <div>
-                {id}. {text}
+            <div onClick={() =>{
+                   this.props.onToggle(id); 
+                }}>   
+              <input type="checkbox" checked={completed}></input>
+              {text}
             </div>
         );
     }
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -1,24 +1,37 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var expect = require('expect');
-var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jquery');
 
-var TodoApp = require("TodoApp");
+var TodoApp = require('TodoApp');
 
-describe("TodoApp", () =>{
-    it("should Exist",() =>{
-        expect(TodoApp).toExist();
-    });
-    
-    it("Should add todo to the todos state on handleAddTodo", ()=>{
-        var todoText = "coding";
-        var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
-        
-        todoApp.setState({todos:[]});
-        todoApp.handleAddTodo(todoText);
-        
-        expect(todoApp.state.todos[0].text).toBe(todoText);
-    });
+describe('TodoApp', () => {
+  it('should exist', () => {
+    expect(TodoApp).toExist();
+  });
+
+  it('should add todo to the todos state on handleAddTodo', () => {
+    var todoText = 'test text';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+    todoApp.setState({todos: []});
+    todoApp.handleAddTodo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  });
+
+  it('should toggle completed value when handleToggle called', () => {
+    var todoData = {
+      id: 11,
+      text: 'Test features',
+      completed: false
+    };
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: [todoData]});
+
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    todoApp.handleToggle(11);
+    expect(todoApp.state.todos[0].completed).toBe(true);
+  });
 });
-

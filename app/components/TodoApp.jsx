18638e80088c74ce934e80304ccba3534d570519
wal faizy <faizy.wali@gmail.com>
2017-02-23 14:47:12 +0400

    Commit with functionality wlocalStorage functionality and their test case

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,39 +1,28 @@
 var React = require("react");
+var uuid = require("node-uuid");
+
+
 var TodoList = require("TodoList");
 var AddTodo = require("AddTodo");
 var TodoSearch = require("TodoSearch");
-var uuid = require("node-uuid");
+var TodoAPI = require("TodoAPI");
+
 
 var TodoApp = React.createClass({
     getInitialState: function() {
         return {
             showCompleted: false,
             searchText: "",
-           todos: [
-               {
-                id: uuid(),
-                text: "Walk the dog",
-                   completed: false
-               }, {
-                id: uuid(),
-                text: "Clean the yard",
-                   completed: true
-               }, {
-                id: uuid(),
-                text: "Leave the mail on the go",
-                   completed: true
-               }, {
-                id: uuid(),
-                text: "Play video games",
-                   completed: false
-               }
-           ]
+            todos: TodoAPI.getTodos()
         };
+    },
+    componentDidUpdate: function() {
+        TodoAPI.setTodos(this.state.todos);
     },
    handleAddTodo: function(text) {
         this.setState({
             todos: [
-                ...this.state.todos, 
+                ...this.state.todos,
                 {
                     id: uuid(),
                     text: text,

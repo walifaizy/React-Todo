1d3d4dc52cc5dd087f7c2e20934f55a417f4f8dc
wal faizy <faizy.wali@gmail.com>
2017-02-24 00:42:07 +0400

    Search Filter Working

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -14,11 +14,37 @@ module.exports = {
     
     try {
         todos = JSON.parse(stringTodos);
-    } catch(e) {
-        
-    }
+    } catch(e) {}
 
     return $.isArray(todos) ? todos : [];
 
-  }
+  },
+    
+    filterTodos: function(todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+        
+        //Filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+        
+         // Filter by searchText
+         filteredTodos = filteredTodos.filter((todo) => {
+         var text = todo.text.toLowerCase();
+         return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+        
+        //Sort Todos with non-completed first
+        filteredTodos.sort((a,b) => {
+            if(!a.completed && b.completed) {
+                return -1;
+            } else if(a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;               
+            }
+        });
+        
+        return filteredTodos;
+    }
 }; 

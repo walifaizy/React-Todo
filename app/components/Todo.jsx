984f1ddae33cbbe962aebfd3a923cf6d93919560
wal faizy <faizy.wali@gmail.com>
2017-02-24 15:40:23 +0400

    Remove bug in date

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,35 +1,31 @@
-var React = require("react");
-var moment = require("moment");
-
+var React = require('react');
+var moment = require('moment');
 
 var Todo = React.createClass({
-    /*toggle: function(id) {
-         this.props.onToggle(id);
-    },*/
-    render: function() {
-        var {id,text,completed,createdAt,completedAt} = this.props;
-        var renderDate = () => {
-            var message = "created: " ;
-            var timestamp = createdAt;
-            
-            if(completed) {
-                message = "completed at ";
-                timestamp = completedAt;
-            }
-            
-            return message + moment.unix(timestamp).format("MMM-Do-YYY @ h:mm a");
-        }
-        
-       return (
-            <div onClick={() =>{
-                   this.props.onToggle(id); 
-                }}>   
-              <input type="checkbox" checked={completed}></input>
-              <span> {text}</span>
-              <p>{renderDate()}</p>
-            </div>
-        );
-    }
+  render: function () {
+    var {id, text, completed, createdAt, completedAt} = this.props;
+    var renderDate = () => {
+      var message = 'Created ';
+      var timestamp = createdAt;
+
+      if (completed) {
+        message = 'Completed ';
+        timestamp = completedAt;
+      }
+
+      return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+    };
+
+    return (
+      <div onClick={() => {
+          this.props.onToggle(id);
+        }}>
+        <input type="checkbox" checked={completed}/>
+        <sapn>{text}</span>
+        <p>{renderDate()}</p>
+      </div>
+    )
+  }
 });
 
 module.exports = Todo;
